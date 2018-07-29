a0f69fe4acee2a9eea7037e190a1d7f197353840
Tom Parker <palfrey@tevp.net>
2018-07-29 23:17:44 +0100

    Automagically have tests for named files

--- tests/file_tests.rs
+++ tests/file_tests.rs
@@ -8,22 +8,14 @@ mod integration {
     use std::io::Read;
     use std;
 
-    #[test]
-    fn fizzbuzz() {
-        file_test("fizzbuzz")
-    }
-
-    #[test]
-    fn minimalist() {
-        file_test("fizzbuzz_minimalist")
-    }
+    include!(concat!(env!("OUT_DIR"), "/test.rs"));
 
     fn file_test(name: &str) {
-        let mut f = File::open(&format!("./tests/{}.rock.expected", name)).unwrap();
+        let mut f = File::open(&format!("./tests/{}.expected", name)).unwrap();
         let mut expected = String::new();
         f.read_to_string(&mut expected).unwrap();
 
-        let args = [&format!("./tests/{}.rock", name)];
+        let args = [&format!("./tests/{}", name)];
         let mut mb = Command::main_binary().unwrap();
         let output = mb.args(&args).output().unwrap();
         let stderr = std::str::from_utf8(&output.stderr).unwrap();
