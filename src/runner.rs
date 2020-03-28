079829cd2537d42e2e6507842a3e644ad5994f11
Tom Parker-Shemilt <palfrey@tevp.net>
2020-03-28 18:37:43 +0000

    Array length is max key number + 1 (because javascript)

--- src/runner.rs
+++ src/runner.rs
@@ -495,7 +495,9 @@ fn get_printable(value: &Expression, state: &State) -> Result<String> {
             };
             get_printable(&v, state)
         }
-        Expression::Array { ref numeric, .. } => Ok(format!("{}", numeric.len())),
+        Expression::Array { ref numeric, .. } => {
+            Ok(format!("{}", numeric.keys().into_iter().max().unwrap() + 1))
+        }
         Expression::ArrayRef {
             ref name,
             ref index,
